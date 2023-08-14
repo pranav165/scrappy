17639101c41242e3d11f17bb1cdf591b418f3fb5
pranav165 <pranav165@gmail.com>
2023-08-14 22:42:43 +0530

    Update settings.py

--- scrape_site/settings.py
+++ scrape_site/settings.py
@@ -93,6 +93,3 @@ CONCURRENT_REQUESTS = 64
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
-RANDOMIZE_DOWNLOAD_DELAY=True
-# Fix 403 httperror
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
